5eab46ff63563460fdc417a2fec3088c43b0e1cc
Ajeet kumar verma <ajeet277001@gmail.com>
2022-12-27 15:43:00 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!-- Header -->
+<?php include "header.php" ?>
+
+<!-- body -->
+<div class="container mt-5">
+    <h1 class="text-center"> Welcome, Lets Create a PHP CRUD Application!</h1>
+        <p class="text-center">
+           Let's perform CRUD (Create, Read, Update, Delete) Application Using PHP.
+        </p>
+  <div class="container">
+    <form action="includes/home.php" method="post">
+        <div class="from-group text-center">
+            <input type="submit" class="btn btn-primary mt-2" value="Let's Do it!">
+        </div>
+    </form>
+  </div>
+</div>
